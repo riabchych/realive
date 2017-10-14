aa36cdab27e4056ca9bfe8822ae5669dab964298
Yevhenii Riabchych <riabchych@gmail.com>
2017-10-14 17:31:04 +0300

    Рефакторинг

--- src/controllers/editCtrl.js
+++ src/controllers/editCtrl.js
@@ -7,47 +7,38 @@
 //
 
 let path = require('path');
+let util = require('util');
+let errors = require('errors');
 let flashMessages = require(path.join(global.config.paths.utils_dir, '/flash'));
 let UserProfile = require(path.join(global.config.paths.models_dir, '/user-profile'));
 let UserModel = require(path.join(global.config.paths.models_dir, '/user'));
+var logger = require(path.join(global.config.paths.utils_dir, '/logger'));
 let Promise = require('bluebird');
 let _ = require('lodash');
 
 module.exports = (req, res) => {
-    if (!_.has(req, 'user') && !_.has(req.user, '_doc.username')) {
+    if (!req.isAuthenticated) {
         req.logout();
         return res.redirect('/login');
-    }
-
-    if (req.method == "POST") {
+    } else if (_.isEqual(req.method, "POST")) {
         params = _.extend(req.query || {}, req.params || {}, req.body || {});
         if (!_.has(params, 'user')) {
             return res.redirect('back');
         } else {
             req.login = Promise.promisify(req.login);
             return new UserModel(req.user).updateUser(params.user)
-                .catch(err => {
-                    console.log('ERROR:', err);
-                })
                 .then(data => {
-                    if (!_.has(data, 'extras.user')) {
-                        throw new Error('Object \'user is\' empty');
-                    } else {
-                        req.login(data.extras.user)
-                            .catch(function (err) {
-                                new Error(err);
-                            })
-                            .then(function () {
-                                res.redirect('back');
-                            });
-                    }
+                    return req.login(data.extras.user);
+                })
+                .then(function () {
+                    return res.redirect('back');
                 })
                 .catch(err => {
-                    console.log('ERROR:', err);
+                    logger.error('ERROR: ' + err.extras.msg);
+                    return res.redirect('back');
                 });
         }
-    }
-    else {
+    } else {
         return res.render('edit', {
             title: 'Редактирование :: ' + req.user.fullName,
             csrfToken: encodeURIComponent(req.csrfToken()),
