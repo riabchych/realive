601f06ac11ba4155ee046da6fdb7e46c9d62767c
Yevhenii Riabchych <riabchych@gmail.com>
2017-10-14 17:32:37 +0300

    Рефакторинг

--- src/controllers/reviewCtrl.js
+++ src/controllers/reviewCtrl.js
@@ -15,47 +15,85 @@ let logger = require(path.join(global.config.paths.utils_dir, '/logger'));
 
 module.exports = (req, res) => {
     if (_.isEqual(req.method, "POST")) {
-        if (_.has(req, 'params.action') && _.isEqual(req.params.action, 'new')) {
-            let params = _.extend(req.body);
-            if (!_.has(params, 'review') && !_.has(params, 'review.to')) {
-                return res.redirect('back');
-            } else {
-                req.body.isNew = true;
-                req.body.to = params.review.to;
-                req.body.from = _.has(req, 'user._doc._id') ? req.user.id : undefined;
-                req.body.isPrivate = params.review.isPrivate || false;
-                req.body.isHidden = _.isEqual(params.review.isHidden, 'on');
-                req.body.isPrivate = _.isEqual(params.review.isPrivate, 'on');
-                req.body.body = params.review.text;
-                req.body.toReview = params.review.toReview;
-                req.sanitizeBody('body').trim();
-                //req.checkBody(schema);
+        if (_.has(req, 'params.action')) {
+            switch (req.params.action) {
+                case 'new':
+                    let params = _.extend(req.body);
+                    if (!_.has(params, 'review') && !_.has(params, 'review.to')) {
+                        return res.redirect('back');
+                    } else {
+                        req.body.isNew = true;
+                        req.body.to = params.review.to;
+                        req.body.from = req.isAuthenticated() && _.has(req, 'user._doc._id') ? req.user.id : undefined;
+                        req.body.isPrivate = params.review.isPrivate || false;
+                        req.body.isHidden = req.isAuthenticated() ? _.isEqual(params.review.isHidden, 'on') : true;
+                        req.body.isPrivate = _.isEqual(params.review.isPrivate, 'on');
+                        req.body.body = params.review.text;
+                        req.body.toReview = params.review.toReview;
+                        req.sanitizeBody('body').trim();
+                        //req.checkBody(schema);
 
-                return req.asyncValidationErrors().then(() => {
-                    return new ReviewModel(req.body).createReview()
-                        .then(result => {
-                            if (_.has(result, 'success') && _.has(result, 'extras.review') && !_.isEmpty(result.extras.review)) {
+                        return req.asyncValidationErrors()
+                            .then(() => {
+                                return new ReviewModel(req.body).createReview();
+                            })
+                            .then(result => {
                                 logger.info("New review has been created");
                                 req.flash('success_messages', 'Review has been sent');
                                 return res.redirect('back');
-                            } else {
-                                throw new Error('Object \'review is\' empty');
-                            }
-                        })
-                        .catch(err => {
-                            throw new Error();
-                        })
-                }).catch(errors => {
-                    _.each(errors, e => {
-                        req.flash('error_messages', e.msg);
-                        logger.debug("validator: " + e.msg);
-                    });
+                            })
+                            .catch(err => {
+                                if (_.isArray(err)) {
+                                    _.each(err, e => {
+                                        req.flash('error_messages', e.msg);
+                                        logger.warn("VALIDATOR: " + e.msg);
+                                    });
+                                } else {
+                                    logger.error(`ERROR: ${err.extras.msg}`);
+                                }
+                                return res.redirect('back');
+                            });
+                    }
+                    break;
+                case 'list':
+                    if (_.has(req.body, 'page') && _.has(req.body, 'uid')) {
+                        let skips = 10 * (_.parseInt(req.body.page) - 1);
+                        return ReviewModel
+                            .readReviews(req.body.uid, skips)
+                            .then(result => {
+                                return res.send(result);
+                            })
+                            .catch(err => {
+                                return res.send(err);
+                            });
+                    } else {
+                        return res.send({
+                            success: false
+                        });
+                    }
+                    break;
+                case 'remove':
+                    if (req.isAuthenticated() && _.has(req.body, 'id')) {
+                        return ReviewModel.delete(req.user.id, req.body.id)
+                            .then(data => {
+                                return res.send(data);
+                            })
+                            .catch(err => {
+                                logger.error(`ERROR: ${err.extras.msg}`);
+                                return res.send(err);
+                            });
+                    } else {
+                        return res.send({
+                            success: false
+                        });
+                    }
+                    break;
+                default:
                     return res.redirect('back');
-                });
+                    break;
             }
         }
+    } else {
+        return res.redirect('back');
     }
-    return res.render('home', {
-        title: "Login"
-    });
 };
