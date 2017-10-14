2683047c3f4c8cc959ae7ec6d0f707dfc73c0402
Yevhenii Riabchych <riabchych@gmail.com>
2017-10-14 17:32:01 +0300

    Рефакторинг

--- src/controllers/loginCtrl.js
+++ src/controllers/loginCtrl.js
@@ -6,36 +6,33 @@
 //  Copyright 2017 Yevhenii Riabchych. All rights reserved.
 //
 
-var passport = require("passport");
+var passport = require('passport');
 var path = require('path');
 var logger = require(path.join(global.config.paths.utils_dir, '/logger'));
 var Promise = require('bluebird');
 var _ = require('lodash');
 
 module.exports = (req, res, next) => {
-    if (_.isEqual(req.method, "POST")) {
-        new Promise((resolve, reject) => {
-                passport.authenticate('login', result => {
-                    return _.has(result, 'extras.user') ? resolve(result.extras.user) : reject();
-                })(req, res, next);
-            })
-            .then(user => {
-                if (!_.has(user, 'username') && _.isEmpty(user.username)) {
-                    return new Promise.reject();
-                } else {
-                    req.login = Promise.promisify(req.login);
-                    return req.login(user)
-                        .then(() => {
-                            logger.info(`User ${user.username} has been logged`);
-                            return res.redirect(`/user/${user.username}`);
-                        });
-                }
-            })
-            .catch(() => {
+    if (_.isEqual(req.method, 'POST')) {
+        passport.authenticate('login', result => {
+            if (result.success) {
+                req.login = Promise.promisify(req.login);
+                return req.login(result.extras.user)
+                    .then(() => {
+                        return res.redirect(`/user/${req.user.username}`);
+                    })
+                    .catch(data => {
+                        logger.error(data.extras.msg);
+                        return res.redirect('back');
+                    });
+            } else {
+                logger.error(result.extras.msg);
                 return res.redirect('back');
-            });
+            }
+        })(req, res, next);
+
     } else {
-        res.render('login', {
+        return res.render('login', {
             'title': 'Login',
             'csrfToken': encodeURIComponent(req.csrfToken())
         });
