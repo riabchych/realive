9c74116ef5375d391b5a45838dc01cfdcfb5210a
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-10-29 21:22:17 +0200

    добавлен строгий режим,  синтаксис es6, изменен експорт

---
+++ src/utils/isLoggedIn.js
@@ -0,0 +1,13 @@
+//
+//  isLoggedIn.js
+//  realive
+//
+//  Created by Yevhenii Riabchych on 2017-09-30.
+//  Copyright 2017 Yevhenii Riabchych. All rights reserved.
+//
+
+'use strict';
+
+export default function (req, res, next) {
+    return req.isAuthenticated() ? next() : res.redirect('/login');
+}
