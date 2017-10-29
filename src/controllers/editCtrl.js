81235a8c99f2de2a5d1b9addbbac4ecd85287ff1
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-10-29 21:14:59 +0200

    добавлен строгий режим,  синтаксис es6, изменен експорт

--- src/controllers/editCtrl.js
+++ src/controllers/editCtrl.js
@@ -6,39 +6,41 @@
 //  Copyright 2017 Yevhenii Riabchych. All rights reserved.
 //
 
-import _ from 'lodash'
-import util from 'util'
-import Promise from 'bluebird'
+'use strict';
+
 import flashMessages from '../utils/flash'
 import UserProfile from '../models/user-profile'
 import UserModel from '../models/user'
 import logger from '../utils/logger'
 
-module.exports = (req, res) => {
+export default (req, res) => {
     if (!req.isAuthenticated) {
-        req.logout()
+        req.logout();
         return res.redirect('/login')
-    } else if (_.isEqual(req.method, "POST")) {
-        params = _.extend(req.query || {}, req.params || {}, req.body || {})
-        if (!_.has(params, 'user')) {
+    }
+    if (Object.is(req.method, 'POST')) {
+        let params = req.body;
+
+        if (!Object.keys(params).includes('user'))
+            return res.redirect('back');
+
+        return new UserModel(req.user).updateUser(params.user).then(data => {
+            return req.logIn(data.extras.user, err => {
+                if (err) {
+                    logger.error(err);
+                    return res.redirect('back')
+                }
+                return res.redirect('back')
+            });
+        }).catch(err => {
+            logger.error('ERROR: ' + err.extras.msg);
             return res.redirect('back')
-        } else {
-            req.login = Promise.promisify(req.login)
-            return new UserModel(req.user).updateUser(params.user).then(data => {
-                return req.login(data.extras.user)
-            }).then(() => {
-                return res.redirect('back')
-            }).catch(err => {
-                logger.error('ERROR: ' + err.extras.msg)
-                return res.redirect('back')
-            })
-        }
-    } else {
-        return res.render('edit', {
-            title: 'Редактирование :: ' + req.user.fullName,
-            csrfToken: encodeURIComponent(req.csrfToken()),
-            messages: flashMessages(req, res),
-            owner: new UserProfile(req.user)
         })
     }
+    return res.render('edit', {
+        title: 'Редактирование :: ' + req.user.fullName,
+        csrfToken: encodeURIComponent(req.csrfToken()),
+        messages: flashMessages(req, res),
+        owner: new UserProfile(req.user)
+    })
 }
