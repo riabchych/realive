201dc0dd3d0a868f29e85d032286ab3019b75d3c
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-10-29 21:20:30 +0200

    добавлен строгий режим,  синтаксис es6, изменен експорт

---
+++ src/services/mailer-service.js
@@ -0,0 +1,53 @@
+//
+//  mailer-service.js
+//  realive
+//
+//  Created by Yevhenii Riabchych on 2017-09-30.
+//  Copyright 2017 Yevhenii Riabchych. All rights reserved.
+//
+
+'use strict';
+
+import pug from 'pug'
+import util from 'util'
+import config from '../config'
+import mailer from '../config/mailer'
+import logger from '../utils/logger'
+
+
+class MailerService {
+    static send(template, mailOptions, templateOptions) {
+        mailOptions.html = this.renderPugFile(__dirname + '/../views/email-templates/' + template, templateOptions);
+
+        logger.debug('[SENDING MAIL]' + util.inspect(mailOptions));
+
+        //if (app.settings.env == 'production') {
+        mailer.sendMail(mailOptions, function (error, response) {
+            if (error) {
+                logger.error(error);
+            } else {
+                logger.info("Message sent: " + response.message);
+            }
+        })
+        //}
+    }
+
+    static sendWelcome(user) {
+        this.send('welcome.pug', {
+            to: user.email,
+            from: config.mailer.from,
+            subject: 'Подтверждение регистрации аккаунта на Realive.com'
+        }, {
+            locals: {
+                user: user
+            }
+        })
+    }
+
+    static renderPugFile(template, options) {
+        let fn = pug.compileFile(template, options);
+        return fn(options.locals)
+    }
+}
+
+export default MailerService
