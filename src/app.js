616889330802b5c1601388af9f35436e697f5d7c
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-19 12:07:57 +0200

    рефакторинг

--- src/app.js
+++ src/app.js
@@ -6,157 +6,163 @@
 //  Copyright 2017 Yevhenii Riabchych. All rights reserved.
 //
 
-import http from 'http'
 import bodyParser from 'body-parser'
+import flash from 'connect-flash'
+import connectMongo from 'connect-mongo'
 import cookieParser from 'cookie-parser'
-import expressSession from 'express-session'
+import csrf from 'csurf'
 import express from 'express'
-import mongoose from 'mongoose'
+import session from 'express-session'
 import expressValidator from 'express-validator'
-import favicon from 'serve-favicon'
+import http from 'http'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
 import passport from 'passport'
+import passportSocketIo from 'passport.socketio'
+import favicon from 'serve-favicon'
+import passportInit from './middleware/passport/init'
+import routes from './routes'
+import socket from './socket'
 import customValidators from './utils/custom-validators'
 import errorHanler from './utils/error-handler'
-import methodOverride from 'method-override'
-import flash from 'connect-flash'
-import csrf from 'csurf'
 import logger from './utils/logger'
-import routes from './routes'
-import morgan from 'morgan'
-import passportInit from './middleware/passport/init'
+
+const MongoStore = connectMongo(session)
 
 class Events {
+  constructor (port) {
+    this.port = Events.normalizePort(port)
+  }
 
-    constructor(port) {
-        this.port = Events.normalizePort(port)
+  static normalizePort (val) {
+    const port = parseInt(val, 10)
+
+    if (isNaN(port)) {
+      return val
     }
 
-    onListening() {
-        logger.info("Server listening on port " + this.port)
+    if (port >= 0) {
+      return port
     }
 
-    static normalizePort(val) {
-        let port = parseInt(val, 10);
+    return false
+  }
 
-        if (isNaN(port)) {
-            return val
-        }
+  onListening () {
+    logger.info('Server listening on port ' + this.port)
+  }
 
-        if (port >= 0) {
-            return port
-        }
-
-        return false
+  onError (error) {
+    if (error.syscall !== 'listen') {
+      throw error
     }
 
-    onError(error) {
-        if (error.syscall !== 'listen') {
-            throw error
-        }
+    const bind = typeof this.port === 'string'
+      ? 'Pipe ' + this.port
+      : 'Port ' + this.port
 
-        let bind = typeof this.port === 'string'
-            ? 'Pipe ' + this.port
-            : 'Port ' + this.port;
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                logger.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                logger.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error
-        }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        logger.error(bind + ' requires elevated privileges')
+        process.exit(1)
+      case 'EADDRINUSE':
+        logger.error(bind + ' is already in use')
+        process.exit(1)
+      default:
+        throw error
     }
+  }
 }
 
 class App extends Events {
-    constructor(config, port = process.env.PORT || '8080') {
-        super(port);
-        this.init(config)
-    }
+  constructor (config, port = process.env.PORT || '8080') {
+    super(port)
+    this.init(config)
+  }
 
-    static initDbUri(config) {
-        return [
-            'mongodb://', config.username
-                ? (config.username + ':' + config.password + '@')
-                : '',
-            config.host,
-            ':',
-            config.port,
-            config.name
-                ? ('/' + config.name)
-                : ''
-        ].join('')
-    }
+  static initDbUri (config) {
+    return [
+      'mongodb://', config.username
+        ? (config.username + ':' + config.password + '@')
+        : '',
+      config.host,
+      ':',
+      config.port,
+      config.name
+        ? ('/' + config.name)
+        : ''
+    ].join('')
+  }
 
-    init(config) {
-        this.app = express();
-        this.app.set('views', __dirname + '/views');
-        this.app.set('view engine', 'pug');
-        this.app.use(favicon(__dirname + '/public/favicon.ico'));
-        this.app.use(morgan('combined', {"stream": logger.stream}));
-        this.app.use(methodOverride('X-HTTP-Method')); // Microsoft
-        this.app.use(methodOverride('X-HTTP-Method-Override')); // Google/GData
-        this.app.use(methodOverride('X-Method-Override')); // IBM
+  init (config) {
+    this.app = express()
+    this.app.set('views', `${__dirname}/views`)
+    this.app.set('view engine', 'pug')
+    this.app.use(favicon(`${__dirname}/public/favicon.ico`))
+    this.app.use(morgan('combined', {'stream': logger.stream}))
+    this.app.use(express.static(`${__dirname}/public`))
+    this.app.use(cookieParser(config.cookies.secret))
+    this.app.use(bodyParser.json())
+    this.app.use(bodyParser.urlencoded({extended: true}))
+    mongoose.Promise = global.Promise
+    mongoose.connection.on('open', ref => {
+      logger.info('Connected to mongo server.')
 
-        let sessionParam = {
-            secret: config.session.secret,
-            saveUninitialized: config.session.saveUninitialized,
-            resave: config.session.resave
-        };
+      let sessionStore = new MongoStore(
+        {mongooseConnection: mongoose.connection})
 
-        this.app.use(express.static(__dirname + '/public'));
-        this.app.use(cookieParser(config.cookies.secret));
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({extended: true}));
-        this.app.use(expressSession(sessionParam));
-        this.app.use(passport.initialize());
-        this.app.use(passport.session());
-        this.app.use(expressValidator({customValidators: customValidators}));
-        this.app.use(flash());
-        this.app.use(csrf({cookie: false}));
-        this.app = errorHanler(this.app);
-        this.app.use((req, res, next) => {
-            let msg = req.flash('success_messages');
-            res.locals.success_messages = msg.length > 0
-                ? msg
-                : null;
-            msg = req.flash('error_messages');
-            res.locals.error_messages = msg.length > 0
-                ? msg
-                : null;
-            next()
-        });
-        passportInit(passport);
-        this.app.use(routes);
+      this.app.use(session({
+        secret: config.session.secret,
+        saveUninitialized: config.session.saveUninitialized,
+        resave: config.session.resave,
+        store: sessionStore
+      }))
+      this.app.use(passport.initialize())
+      this.app.use(passport.session())
+      this.app.use(expressValidator({customValidators: customValidators}))
+      this.app.use(flash())
+      this.app.use(csrf({cookie: false}))
+      this.app = errorHanler(this.app)
+      this.app.use((req, res, next) => {
+        let msg = req.flash('success_messages')
+        res.locals.success_messages = msg.length > 0
+          ? msg
+          : null
+        msg = req.flash('error_messages')
+        res.locals.error_messages = msg.length > 0
+          ? msg
+          : null
+        next()
+      })
+      passportInit(passport)
+      this.app.use(routes)
 
-        this.app.on('connection', socket => {
-            socket.setNoDelay()
-        });
+      this.app.on('connection', socket => {
+        // socket.setNoDelay()
+      })
 
-        mongoose.connection.on('open', ref => {
-            logger.info('Connected to mongo server.')
-        });
+      this.server = http.createServer(this.app)
 
-        mongoose.connection.on('error', err => {
-            logger.warn('Could not connect to mongo server!');
-            logger.error(err)
-        });
+      socket(this.server, passportSocketIo.authorize({
+        key: 'connect.sid',
+        secret: config.session.secret,
+        store: sessionStore,
+        passport: passport,
+        cookieParser: cookieParser
+      }))
 
-        mongoose.connect(App.initDbUri(config.db), {useMongoClient: true});
-        return this.app
-    }
+      this.server.listen(this.port)
+      this.server.on('listening', this.onListening.bind(this))
+      this.server.on('error', this.onError.bind(this))
+    })
 
-    run() {
-        this.server = http.createServer(this.app).listen(this.port);
-        this.server.on('listening', this.onListening.bind(this));
-        this.server.on('error', this.onError.bind(this))
-    }
+    mongoose.connection.on('error', err => {
+      logger.error(err)
+    })
+
+    mongoose.connect(App.initDbUri(config.db), {useMongoClient: true})
+  }
 }
 
 export default App
--- src/config/api-messages.js
+++ src/config/api-messages.js
@@ -6,22 +6,22 @@
 //  Copyright 2017 Yevhenii Riabchych. All rights reserved.
 //
 
-'use strict';
+'use strict'
 
-let ApiMessages = {};
+const ApiMessages = {}
 
-ApiMessages.EMAIL_NOT_FOUND = 0;
-ApiMessages.INVALID_PWD = 1;
-ApiMessages.DB_ERROR = 2;
-ApiMessages.NOT_FOUND = 3;
-ApiMessages.EMAIL_ALREADY_EXISTS = 4;
-ApiMessages.COULD_NOT_CREATE_USER = 5;
-ApiMessages.PASSWORD_RESET_EXPIRED = 6;
-ApiMessages.PASSWORD_RESET_HASH_MISMATCH = 7;
-ApiMessages.PASSWORD_RESET_EMAIL_MISMATCH = 8;
-ApiMessages.COULD_NOT_RESET_PASSWORD = 9;
-ApiMessages.PASSWORD_CONFIRM_MISMATCH = 10;
-ApiMessages.INVALID_USERNAME = 11;
-ApiMessages.INTERNAL_ERROR = 12;
+ApiMessages.EMAIL_NOT_FOUND = 0
+ApiMessages.INVALID_PWD = 1
+ApiMessages.DB_ERROR = 2
+ApiMessages.NOT_FOUND = 3
+ApiMessages.EMAIL_ALREADY_EXISTS = 4
+ApiMessages.COULD_NOT_CREATE_USER = 5
+ApiMessages.PASSWORD_RESET_EXPIRED = 6
+ApiMessages.PASSWORD_RESET_HASH_MISMATCH = 7
+ApiMessages.PASSWORD_RESET_EMAIL_MISMATCH = 8
+ApiMessages.COULD_NOT_RESET_PASSWORD = 9
+ApiMessages.PASSWORD_CONFIRM_MISMATCH = 10
+ApiMessages.INVALID_USERNAME = 11
+ApiMessages.INTERNAL_ERROR = 12
 
 export default ApiMessages
