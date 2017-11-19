95132e9c07cfeb4a3663593a248c7049312b7eb9
Yevhenii Riabchych <evgeny.ryabchich@gmail.com>
2017-11-19 12:08:17 +0200

    удален bluebird

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -6,19 +6,18 @@
 //  Copyright 2017 Yevhenii Riabchych. All rights reserved.
 //
 
-'use strict';
+'use strict'
 
 import nodemailer from 'nodemailer'
-import Promise from 'bluebird'
 import config from './index'
 
-let transporter = nodemailer.createTransport({
-    host: config.mailer.host,
-    port: config.mailer.port,
-    auth: {
-        user: config.mailer.auth.user,
-        pass: config.mailer.auth.pass
-    }
-});
+const transporter = nodemailer.createTransport({
+  host: config.mailer.host,
+  port: config.mailer.port,
+  auth: {
+    user: config.mailer.auth.user,
+    pass: config.mailer.auth.pass,
+  },
+})
 
-export default Promise.promisifyAll(transporter)
+export default transporter
